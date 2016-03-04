563b4134e63bfa43e585ea786c8e31eba0bbae4f
ronald <roal99@gamil.com>
2016-03-04 10:25:19 -0500

    creando conocimientos

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+
+		<div class="contenedor">
+
+     <nav>
+     	<ul class="menu-lista">
+     		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+     		<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+     		<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+     		<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+     		<li class="menu-item"><a href="" class="menu-link">CONTACTENOS</a></1>
+     		
+     	</ul>
+
+     </nav>
+
+     <h2 class="rojo">EMPRESAS MAS ADMIRADAS DEL MUNDO</h2>
+     <h2 class="verde">APLE</h2>
+
+    	<p class="negro"> La compañía fundada por Steve Jobs y basada en Cupertino, California, es la de una empresa creativa y que marca tendencias en el mundo de la tecnología. Sus dispositivos han revolucionado la manera en que nos relacionamos tanto con la tecnología como con el mundo que nos rodea. A pesar del fracaso de su sistema de mapas, el iPhone sigue siendo el smartphone líder del mercado, y a pesar de la competencia de Samsung.</p>
+
+
+     <img src="6.jpg" height="300" width="900">
+
+     <h2 class="morado">GOOGLE</h2>
+    <p class="negro">El gigante de la información ha tenido un viraje interesante en los últimos meses y se ha enfocado más en expandir su marca a dispositivos de hardware como el revolucionario sistema Google Glass, así como la adquisición de varias compañías que fabrican y desarrollan robots. Esta empresa busca transformar al mundo a través de la información.</p>	
+
+     <div class="slider">
+
+     <img class="digna" src="1.jpg" height="180" width="180">
+     <img class="digna"src="7.jpg" height="180" width="180">
+     <img class="digna"src="4.jpg" height="180" width="180">
+     <img class="digna"src="5.jpg" height="180" width="180">
+
+	</div>
+
+	</div>
+	
+</body>
+</html>
